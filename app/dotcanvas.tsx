b53d47cfbbd8ace4bcf88ad8636279c2dde5fa02
DevonCallanan <devoncallanan@gmail.com>
2023-06-05 21:55:06 -0400

    feat: add responsive design to application

--- app/dotcanvas.tsx
+++ app/dotcanvas.tsx
@@ -9,6 +9,7 @@ interface DotCanvasProps {
 
 export function DotCanvas(props: DotCanvasProps) {
   const [dotsImage, setDotsImage] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
   const [converted, setConverted] = useState<boolean>(false);
 
   useEffect(() => {
@@ -33,27 +34,45 @@ export function DotCanvas(props: DotCanvasProps) {
       "data:image/png;base64," + lambdaResponse.img.substring(2, lambdaResponse.img.length - 1)
     );
     setConverted(true);
+    setLoading(false);
   };
 
   return (
     <div className="flex grid relative justify-center">
+      <div className="sm:absolute z-10 row-auto items-center self-center justify-self-center m-4">
+        {props.configComplete &&
+          !converted &&
+          (loading ? (
+            <Image alt="loading" src="/Loading_icon.gif" width={50} height={50} />
+          ) : (
+            <button
+              className={
+                "rounded-full border-solid border-4 \
+         bg-white hover:border-grey-700 text-black \
+         font-bold py-2 px-4"
+              }
+              onClick={() => {
+                handleConvertImage();
+                setLoading(true);
+              }}
+            >
+              CONVERT
+            </button>
+          ))}
+      </div>
       <div className="row-auto">
         {dotsImage ? (
           <img src={dotsImage} key={dotsImage} width={500} height={500} />
         ) : props.templateImage ? (
-          <img src={props.templateImage} key={props.templateImage} width={500} height={500} />
+          <img
+            className={loading ? "opacity-50" : ""}
+            src={props.templateImage}
+            key={props.templateImage}
+            width={500}
+            height={500}
+          />
         ) : (
           <Image alt={"devon looking cool"} src="/blank.jpg" width={500} height={500} />
-        )}
-      </div>
-      <div className="absolute z-10 row-auto items-center self-center justify-self-center m-4">
-        {props.configComplete && !converted && (
-          <button className={
-            "rounded-full border-solid border-4 \
-         bg-white hover:border-grey-700 text-black \
-         font-bold py-2 px-4"} onClick={handleConvertImage}>
-            CONVERT
-          </button>
         )}
       </div>
     </div>
