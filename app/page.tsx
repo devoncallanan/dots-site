0406e918dc0230549eba2b45d78aa6a49f4e96bd
DevonCallanan <devoncallanan@gmail.com>
2023-09-14 01:29:22 -0400

    Remove stepper UI to improve usability

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,31 @@
 "use client";
 
 import React, { useState } from "react";
+import { ConfigForm } from "./configform";
 import { DotCanvas } from "./dotcanvas";
-import { StepDefinition, Stepper, ImagePicker, BrushSelector } from "./stepper";
 
 export default function DotGenerator() {
   const [templateImage, setTemplateImage] = useState<string | null>(null);
   const [brushUrl, setBrushUrl] = useState<string | null>(null);
-  const [stepsComplete, setStepsComplete] = useState<boolean>(false);
-
-  const steps: Array<StepDefinition> = [
-    {
-      title: "first",
-      renderContents: (active, setCanComplete) => (
-        <ImagePicker
-          handleFileSelect={setTemplateImage}
-          setCanComplete={setCanComplete}
-          active={active}
-        />
-      ),
-    },
-    {
-      title: "second",
-      renderContents: (active, setCanComplete) => (
-        <BrushSelector
-          handleSelectBrush={(brush) => setBrushUrl(brush)}
-          setCanComplete={setCanComplete}
-          active={active}
-        />
-      ),
-    },
-  ];
 
   return (
-    <main className="grid lg:flex min-h-screen row-auto items-center p-24">
-      <div className="basis-1/3">
-        <Stepper
-          steps={steps}
-          direction="horizontal"
-          initialIndex={0}
-          setStepsComplete={setStepsComplete}
-        />
+    <main >
+      <div className="absolute left-0 right-0 text-center m-2 text-3xl">
+          Dot Art!!
       </div>
-      <div className="basis-2/3">
-        <DotCanvas
-          templateImage={templateImage}
-          configComplete={stepsComplete}
-          brushUrl={brushUrl}
-        />
+      <div className="p-10 lg:p-48 h-screen flex">
+        <div className=' grow rounded-md border-solid border-4 \
+        grid lg:flex row-auto items-center h-full'>
+          <div className="basis-1/3 h-full bg-slate-50">
+            <ConfigForm onSetBrush={setBrushUrl} onSetTemplate={setTemplateImage}/>
+          </div>
+          <div className="basis-2/3 h-full">
+            <DotCanvas
+              templateImage={templateImage}
+              brushUrl={brushUrl}
+            />
+          </div>
+        </div>
       </div>
     </main>
   );
